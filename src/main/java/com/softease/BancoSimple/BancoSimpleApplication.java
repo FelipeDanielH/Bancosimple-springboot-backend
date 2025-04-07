341ebda08139ee9d958e6c65130cd499f6b03672
Felipe Henriquez <felipe.daniel.henriquez@gmail.com>
2025-04-07 09:53:08 -0400

    first commit

---
+++ src/main/java/com/softease/BancoSimple/BancoSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.softease.BancoSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softease/BancoSimple/BancoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softease.BancoSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
